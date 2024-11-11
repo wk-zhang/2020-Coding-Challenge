b9c371c6be47e3b06b6cab6176eb203bd6326169
wk-zhang <kevzha@outlook.com>
2024-11-11 11:38:25 -0500

    modify server.py & scoreboard.js to make frondend responsive

--- server.py
+++ server.py
@@ -51,6 +51,8 @@ def increase_score():
         if team["id"] == team_id:
             team["score"] += 1
 
+    # sort the teams in non-increasing order of scores
+    scoreboard.sort(key=lambda x: x["score"], reverse=True)
     return jsonify(scoreboard=scoreboard)
 
 
